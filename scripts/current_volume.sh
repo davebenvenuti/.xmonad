7f3a6d2e61be8ba8a299c61e7d8ff92f5ea0645f
Dave Benvenuti <davebenvenuti@gmail.com>
2015-05-02 14:21:38 -0400

    fix current_volume.sh for ubuntu vivid

--- scripts/current_volume.sh
+++ scripts/current_volume.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-pactl list sinks | grep "Volume: 0" | tail -1 | awk -F : '{ print $4 }' | sed -e 's/^[[:space:]]*//'
+lsb_release -a | grep vivid > /dev/null
+
+if [ "$?" == "0" ]; then
+    pactl list sinks | grep -A10 analog-stereo | grep -P '^\W+Volume:' | grep -Po '\d{1,3}%' | head -1
+else
+    pactl list sinks | grep "Volume: 0" | tail -1 | awk -F : '{ print $4 }' | sed -e 's/^[[:space:]]*//'
+fi
 
