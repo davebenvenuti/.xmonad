4a1f0517bec63741b5c6a652a8319670f47be28e
Dave Benvenuti <davebenvenuti@gmail.com>
2017-04-21 18:05:45 -0700

    We don't need to check linux distrobution anymore

--- scripts/current_volume.sh
+++ scripts/current_volume.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-lsb_release -a | grep "yakkety" > /dev/null
-
-if [ "$?" == "0" ]; then
-    pactl list sinks | grep -A10 analog-stereo | grep -P '^\W+Volume:' | grep -Po '\d{1,3}%' | head -1
-else
-    pactl list sinks | grep "Volume: 0" | tail -1 | awk -F : '{ print $4 }' | sed -e 's/^[[:space:]]*//'
-fi
-
+pactl list sinks | grep -A10 analog-stereo | grep -P '^\W+Volume:' | grep -Po '\d{1,3}%' | head -1
